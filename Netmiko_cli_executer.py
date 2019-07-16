ecf94d69df76cc91da0cd4d932e4d7f58fa8cc65
surenderreddychitteddy <52309469+surenderreddychitteddy@users.noreply.github.com>
2019-07-16 10:43:23 +0530

    Update Netmiko_cli_executer.py

--- Netmiko_cli_executer.py
+++ Netmiko_cli_executer.py
@@ -24,6 +24,21 @@ def cli_executer(connection, command, device_type):
     '''send_command_timing() is used to send command and receive output but sometimes it fails to wait till it receives device prompt.'''
     elif device_type == 'fw':
         output = connection.send_command_timing(command)
+        
+    elif device_type == 'router' and 'traceroute' in command:
+        logging.info('Please wait for 30 seconds while Script is performing Traceroute test')
+        connection.write_channel(command + '\n')
+        i = 0
+        while i < 30:
+            i += 1
+            print(f"{i} sec.")
+            time.sleep(1)
+
+        logging.info("Executing \"Ctrl + Shift + 6\" to abort traceroute if it's not over within 30 seconds")
+        ctrl_shift_6 = chr(30)
+        connection.write_channel(ctrl_shift_6)
+
+        output = connection.read_channel()
     else:
         logging.error('Please enter the device type as lb or fw')
     print(output)
